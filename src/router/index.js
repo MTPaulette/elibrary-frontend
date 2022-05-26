59f4d035884e50014f4ffa81702b5eda04c44ccb
jordankraft <jordan.ouabo@facsciences-uy1.com>
2022-05-26 15:08:09 +0100

    Création de la première page filière: informatique

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import LoginPage from "../views/Authentification/Login.vue";
+import Informatiques from "../views/Pages/Filiere/Informatiques.vue";
 
 Vue.use(VueRouter);
 
@@ -11,6 +12,12 @@ const routes = [
     name: "Home",
     component: Home,
   },
+  {
+    path: "/informatiques",
+    name: "Informatiques",
+    component: Informatiques,
+  },
+
   {
     path: "/about",
     name: "About",
---
+++ src/views/Pages/Filiere/Informatiques.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <navbar></navbar>
+        <informatique></informatique>
+    </div>
+</template>
+
+<script>
+import Informatique from "@/components/Filiere/Informatique.vue";
+import Navbar from "@/components/Acceuil/Navbar.vue";
+//import Navbar from '../../../components/Acceuil/Navbar.vue';
+export default {
+    name : 'Informatique',
+    components:{
+        'informatique' : Informatique,
+        'navbar' : Navbar,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
